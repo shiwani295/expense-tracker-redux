e46c201bb4116fb8603536322b685cbbedb028fd
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-04-30 12:44:41 +0530

    Signup Login in redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/NavBar/Header";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Products from "./Components/Pages/Products";
+import AboutUs from "./Components/Pages/AboutUs";
+import SignUp from "./Components/SignUp";
+import { useDispatch, useSelector } from "react-redux";
+import { authAction } from "./Store";
+// import { useEffect } from "react";
 
 function App() {
+  const isauth = useSelector((state) => state.isAuthentication);
+  console.log(isauth);
+  const dispatch = useDispatch();
+  // const token = localStorage.getItem("token");
+  // useEffect(() => {
+  //   if (token) {
+  //     dispatch(authAction.login());
+  //   }
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route
+          path="/"
+          element={isauth ? <Navigate to="/home" /> : <SignUp />}
+        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </>
   );
 }
 
