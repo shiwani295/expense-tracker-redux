fe1fad556e1b15dc430f0681ef4f9a68595adaef
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-05-10 12:53:03 +0530

    Delete data from Expense list table

--- src/Store/MyExpenseSlice.js
+++ src/Store/MyExpenseSlice.js
@@ -12,8 +12,14 @@ const MyExpenseSlice = createSlice({
     addExpense(state, action) {
       state.expense = action.payload;
     },
+    getExpense(state, action) {
+      state.expense = action.payload.expense;
+      state.totalAmount = action.payload.totalAmount;
+    },
   },
 });
 
-export const MyExpenseAction = MyExpenseSlice.actions;
+export const MyExpenseAction = MyExpenseSlice.actions; // add axpense funtion and myexpense.js index.js, these for add expensef orm
+export const getExpenseAction = MyExpenseSlice.actions; //getexpense table expense.js index.js for show data into table
+// export const getExpenseAction = MyExpenseSlice.actions;
 export default MyExpenseSlice.reducer;
