8f9eef72c3f7a18c07189d6dbcf2377165a3af2b
Shivani tiwari <52812394+shiwani295@users.noreply.github.com>
2023-07-24 20:05:34 +0000

    -- to deploy

--- src/Components/CSV/AsycCSV.js
+++ src/Components/CSV/AsycCSV.js
@@ -1,7 +1,43 @@
+// import React from "react";
+// import { CSVLink } from "react-csv";
+// import { useSelector } from "react-redux";
+// import "./AsycCSV.css";
+
+// const header = [
+//   { label: "Amount", key: "amount" },
+//   { label: "Category", key: "category" },
+//   { label: "Date", key: "date" },
+//   { label: "Description", key: "description" },
+// ];
+
+// const AsycCSV = () => {
+//   const data = useSelector((state) => state.expense.expense);
+//   const ExpenseData = data && data.map((item) => item.exprenseData);
+
+//   //make sure the data come with this format [{},{},{}]
+//   const csvReport = {
+//     data: ExpenseData,
+//     headers: header,
+//     filename: "Expense_Report.csv",
+//   };
+//   return (
+//     <div>
+//       <button className="btn btn-outline border bg-white border-primary btn-sm buttonCSV mt-4 ml-3 ">
+//         <CSVLink {...csvReport} style={{ textDecoration: "none" }}>
+//           <i class="fa fa-download" aria-hidden="true"></i> Download CSV
+//         </CSVLink>
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default AsycCSV;
+
+
 import React from "react";
 import { CSVLink } from "react-csv";
 import { useSelector } from "react-redux";
-import "../CSV/AsycCSV.css";
+import "./AsycCSV.css";
 
 const header = [
   { label: "Amount", key: "amount" },
@@ -10,31 +46,43 @@ const header = [
   { label: "Description", key: "description" },
 ];
 
-const AsycCSV = () => {
+const AsyncCSV = () => {
   const data = useSelector((state) => state.expense.expense);
-  console.log(data);
-  const ExpenseData = data && data.map((item) => item.exprenseData);
-  // data.map((item) => {
-  console.log(ExpenseData);
-  // });
 
-  //make sure the data come with this formate [{},{},{}]
+  // Make sure the data is an array of objects with keys "amount", "category", "date", and "description"
+  const isValidData =
+    Array.isArray(data) &&
+    data.length > 0 &&
+    data.every(
+      (item) =>
+        typeof item === "object" &&
+        "amount" in item &&
+        "category" in item &&
+        "date" in item &&
+        "description" in item
+    );
+
+  // If data is not in the correct format, show an error message or handle it as needed
+  if (!isValidData) {
+    return <div>Error: Data is not in the correct format.</div>;
+  }
+
   const csvReport = {
-    data: ExpenseData,
+    data: data,
     headers: header,
     filename: "Expense_Report.csv",
   };
+
   return (
     <div>
-      {/* type="button"
-        className="btn btn-outline-primary btn-sm buttonCSV mt-4 ml-3" */}
-      <button className="btn btn-outline border bg-white border-primary btn-sm buttonCSV mt-4 ml-3 ">
+      <button className="btn btn-outline border bg-white border-primary btn-sm buttonCSV mt-4 ml-3">
         <CSVLink {...csvReport} style={{ textDecoration: "none" }}>
-          <i class="fa fa-download" aria-hidden="true"></i> Download CSV
+          <i className="fa fa-download" aria-hidden="true"></i> Download CSV
         </CSVLink>
       </button>
     </div>
   );
 };
 
-export default AsycCSV;
+export default AsyncCSV;
+
